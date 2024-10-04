bb98634f0bccd25fb8dd6d26b77e3eed199ad041
Filda4515 <48486481+Filda4515@users.noreply.github.com>
2024-10-04 18:20:56 +0200

    TelnetClient implementation

--- app/src/main/java/utb/fai/TelnetClient.java
+++ app/src/main/java/utb/fai/TelnetClient.java
@@ -16,10 +16,57 @@ public class TelnetClient {
     public void run() {
         try {
             Socket socket = new Socket(serverIp, port);
-            // Implementation of receiving and sending data
-            // Implement processing of input from the user and sending data to the server
-            // Implement response processing from the server and output to the console
+
+            BufferedReader userReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+            Thread inputThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        String userInput;
+                        while ((userInput = userReader.readLine()) != null) {
+                            writer.println(userInput);
+                            if (userInput.equals("/QUIT")) {
+                                break;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Thread outputThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        String serverResponse;
+                        while ((serverResponse = reader.readLine()) != null) {
+                            System.out.println(serverResponse);
+                            if (serverResponse.equals("/QUIT")) {
+                                break;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            inputThread.start();
+            outputThread.start();
+
+            inputThread.join();
+            outputThread.join();
+
+            reader.close();
+            writer.close();
+            socket.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
